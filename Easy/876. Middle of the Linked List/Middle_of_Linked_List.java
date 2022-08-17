149f74045ce13c45a0addf0807828c6a1b113eff
Johnnyded <johnny.p828@yahoo.com>
2022-08-16 18:49:19 -0700

    Added 876. Middle of Linked List

---
+++ Easy/876. Middle of the Linked List/Middle_of_Linked_List.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ * 
+ * Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+Explanation: 
+Using a two pointer method, we set both to the head of the list. Then we have both iterate
+through the list whilst the fast one goes twice as fast as the slow pointer. We stop when
+fast pointer reaches the end meaning the slow pointer will be in the middle at that time.
+return the slow pointer for the answer.
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
